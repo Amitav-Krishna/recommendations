410fd44971956ba2555b7e4369c7a725cbf95486
Amitav-Krishna <75227207+Amitav-Krishna@users.noreply.github.com>
2025-04-18 12:09:13 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["amitavkrishna.com"],
   basePath: "/review",
-  devServerPort: 3100,
 };
 
 export default nextConfig;
